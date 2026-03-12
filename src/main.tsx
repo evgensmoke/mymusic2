07402077856ffdf35d5a2338ae2401278db1dd28
evgensmoke <evgen1285@gmail.com>
2026-03-12 18:38:47 +0300

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
+// Добавляем аварийный вывод ошибок на экран
+window.onerror = function(msg, url, line) {
+  alert("ОШИБКА: " + msg + "\nГде: " + url + "\nСтрока: " + line);
+  return false;
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
